c0cdce2a3bdc8e094b25659989ad38edbd979e55
unhang11@gmail.com <unhang11@gmail.com>
2022-03-02 21:21:25 +0700

    add post API

--- app/index.js
+++ app/index.js
@@ -7,8 +7,17 @@ const HTTP_PORT = process.env.HTTP_PORT || 3001;
 const app = express();
 const bc = new BlockChain();
 
+app.use(bodyParser.json());
+
 app.get("/blocks", (req, res, next) => {
   res.status(200).json(bc.chain);
+});
+
+app.post("/mine", (req, res) => {
+  const block = bc.addBlock(req.body.data);
+  console.log(`New Block added: ${block.toString()}`);
+  // res.status(201).json(block);
+  res.redirect("/blocks");
 });
 
 app.listen(HTTP_PORT, () =>
