99654a11b4aaac1f17e72dc2381f723e50559989
unhang11@gmail.com <unhang11@gmail.com>
2022-03-03 21:26:45 +0700

    Create p2p server

---
+++ app/p2p-server.js
@@ -0,0 +1,21 @@
+const Websocket = require("ws");
+
+const P2P_PORT = process.env.P2P_PORT || 5001;
+const peers = process.env.PEERS ? process.env.PEERS.split(",") : [];
+// const peers;
+
+class P2PServer {
+  constructor(blockchain) {
+    this.blockchain = blockchain;
+    this.sockets = [];
+  }
+
+  listen() {
+    const server = new Websocket.Server({ port: P2P_PORT });
+    server.on("connection", (socket) => this.connectSocket(socket));
+  }
+
+  connectSocket(socket) {
+    this.sockets.push(socket);
+  }
+}
