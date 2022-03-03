13c1bc4d6a9a9f931401232bd895ebe16d35eff6
unhang11@gmail.com <unhang11@gmail.com>
2022-03-03 22:08:14 +0700

    start p2p server

--- app/index.js
+++ app/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const BlockChain = require("../blockchain");
+const P2PServer = require("./p2p-server");
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 const bc = new BlockChain();
+const p2pServer = new P2PServer(bc);
 
 app.use(bodyParser.json());
 
@@ -23,3 +25,5 @@ app.post("/mine", (req, res) => {
 app.listen(HTTP_PORT, () =>
   console.log(`server is running on port ${HTTP_PORT}`)
 );
+
+p2pServer.listen();
--- app/p2p-server.js
+++ app/p2p-server.js
@@ -2,7 +2,7 @@ const Websocket = require("ws");
 
 const P2P_PORT = process.env.P2P_PORT || 5001;
 const peers = process.env.PEERS ? process.env.PEERS.split(",") : [];
-// const peers;
+// a peer is an address, look like ws://localhost:12345
 
 class P2PServer {
   constructor(blockchain) {
@@ -13,9 +13,21 @@ class P2PServer {
   listen() {
     const server = new Websocket.Server({ port: P2P_PORT });
     server.on("connection", (socket) => this.connectSocket(socket));
+    this.connectToPeers();
+    console.log(`Listening for peer-to-peer connections on: ${P2P_PORT}`);
+  }
+
+  connectToPeers() {
+    peers.forEach((peer) => {
+      const socket = new Websocket(peer);
+      socket.on("open", () => this.connectSocket(socket));
+    });
   }
 
   connectSocket(socket) {
     this.sockets.push(socket);
+    console.log("Socket connected");
   }
 }
+
+module.exports = P2PServer;
