e2a14e0e7104e8c6b14f8b8fd17c006bc96e6e36
unhang11@gmail.com <unhang11@gmail.com>
2022-03-04 23:09:02 +0700

    create Wallet class

---
+++ wallet/index.js
@@ -0,0 +1,18 @@
+const { INITIAL_BALANCE } = require("../config");
+
+class Wallet {
+  constructor() {
+    this.balance = INITIAL_BALANCE;
+    this.keyPair = null;
+    this.publicKey = null;
+  }
+
+  toString() {
+    return `Wallet
+    publicKey  : ${this.publicKey}
+    balance    : ${this.balance}
+    `;
+  }
+}
+
+module.exports = Wallet;
